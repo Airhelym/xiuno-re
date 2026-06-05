420df975e1731f049845878619fec4b461949d88
Anonymous <anonymous@example.com>
2026-06-05 17:38:24 +0800

    2.0.0-b5

--- log/202507/admin_login_error.php
+++
@@ -1,2 +0,0 @@
-<?php exit;?>	2025-07-31 21:35:52	127.0.0.1	/admin/?other-cache.htm	1	password error. uid:1 - ******
-<?php exit;?>	2025-07-31 21:35:54	127.0.0.1	/admin/?other-cache.htm	1	password error. uid:1 - ******
--- log/202508/db_error.php
+++
@@ -1,1 +0,0 @@
-<?php exit;?>	2025-08-06 16:00:57	127.0.0.1	/install/action=db	0	SQL: errno: 1045, errstr: 连接数据库服务器失败:SQLSTATE[HY000] [1045] Access denied for user 'root'@'localhost' (using password: YES)
